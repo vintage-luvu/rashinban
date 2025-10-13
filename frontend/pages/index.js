77db89c5759f4161dd4b5e4b9ab2f6282e4ec7a8
vintage-luvu <hal.mail.like@gmail.com>
2025-10-13 20:13:53 +0900

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,15 +5,16 @@ import React from "react";
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
 export default function Home() {
+  const data = [
+    { x: [1, 2, 3], y: [2, 6, 3], type: "scatter", mode: "lines+markers", marker: { color: "red" } },
+  ];
+
+  const layout = { width: 600, height: 400, title: "A Fancy Plot" };
+
   return (
     <div>
       <h1>サンプルグラフ</h1>
-      <Plot
-        data={[
-          { x: [1, 2, 3], y: [2, 6, 3], type: "scatter", mode: "lines+markers", marker: { color: "red" } },
-        ]}
-        layout={{ width: 600, height: 400, title: "A Fancy Plot" }}
-      />
+      <Plot data={data} layout={layout} />
     </div>
   );
 }
